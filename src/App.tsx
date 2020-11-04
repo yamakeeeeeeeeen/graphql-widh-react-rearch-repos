b496f5fd5205641f27571eed9519cf84a5e66bb8
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2020-11-04 10:07:11 +0900

    feat: setup dotenv

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+console.log("GITHUB_TOKEN", GITHUB_TOKEN);
+
 const App = () => <div>Hello, GraphQL</div>;
 
 export default App;
